6fd1d3ce672dfa852365f78a0021b8f5ce5b0a3f
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-23 12:54:12 +0200

    Testimonials and Products

--- src/componets/SlideCard/SlideCard.jsx
+++ src/componets/SlideCard/SlideCard.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-const SlideCard = () => {
+const SlideCard = ({ imageURL, text }) => {
   return (
     <section className="border-2 border-red-500 md:h-full md:w-[50%] md:flex md:flex-col md:justify-center">
       <section className="border-2 border-green-300 h-[80%]">
         <img
-          src="products/1.PNG"
+          src={imageURL}
           alt=""
           className="h-full w-full object-cover object-center"
         />
       </section>
-      <section className="p-4 border-2 border-blue-600">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. accusamus?
-      </section>
+      <section className="p-4 border-2 border-blue-600">{text}</section>
     </section>
   );
 };
