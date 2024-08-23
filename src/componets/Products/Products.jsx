14d509b88ba8804eabbb09f3fcb437cab1c743d9
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-23 12:20:55 +0200

    Slide Card: Complete

--- src/componets/Products/Products.jsx
+++ src/componets/Products/Products.jsx
@@ -4,12 +4,15 @@ import React from "react";
 const Products = () => {
   return (
     <section className="h-screen flex flex-col justify-center ">
-      <section className="flex flex-col justify-center sm:flex-row sm:items-center p-5 border-2 border-green-600">
-        <section className="border-2 border-black p-4 h-[60%] sm:flex sm:flex-col justify-center">
+      <section className="flex flex-col justify-center sm:flex-row md:items-center md:h-full gap-5 p-5">
+        <section className="border-2 border-black p-4 flex-col justify-center  md:flex md:h-[50%] md:w-[20%]">
           <section className="mb-4">The Collection</section>
-          <section>The Art of Cognac</section>
-          <button>Discover the entire collection</button>
+          <section className="text-2xl">The Art of Cognac</section>
+          <button className="mr-auto my-3">
+            Discover the entire collection
+          </button>
         </section>
+
         <SlideCard />
       </section>
     </section>
