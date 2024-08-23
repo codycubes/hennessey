c1fe45da0ef08452ead92f132c2ff10536916b52
mbali92 <83025186+mbali92@users.noreply.github.com>
2024-08-23 13:16:59 +0200

    commit sec

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -1,36 +1,66 @@
 import React from "react";
-
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return <div>
-    <footer className="bg-[#D9D9D9] p-4 w-1280  py-0">
-        <div className="flex items-center justify-between py-8 px-8 text-black">
-
+    <footer className="p-[500px] w-1280 h-[100px] py-0">
+     
+        <div className="inline-flex  justify-start items-center w-100 py-8 px-8 text-black">
+       {/* <p> © 2024 HENNESSY</p> */}
+          <ul className="flex items-center gap-4 py-2 mx-auto ">
+            <li className="px-10 ">
+              <a href="#"> HOME</a>
+            </li>
+            <li className="px-10">
+              <a href="#">COLLECTION</a>
+          </li>
+          <li className="px-10">
+              <a href="#">ABOUT</a>
+          </li>
+          <li className="px-10">
+              <a href="#">TESTIMONIAL</a>
+          </li>
+          <li className="px-10">
+              <a href="#">CONTACT</a>
+            </li>
+        </ul>
+        </div>
+            </footer>
+        <div className="flex sm:  items-center w-100 justify-between py-8 px-8 text- ">
           <ul className="flex items-center gap-4 py-2 mx-auto ">
             <li className="px-4 ">
-              <a href="#"></a>
+               <a href="#">CONTACT US </a>
             </li>
             <li className="px-4">
-              <a href="#"></a>
+              <a href="#">FAQs</a>
+          </li>
+          <li className="px-4">
+              <a href="#">INFORMATION NOTE AND COOKIES</a>
+          </li>
+          <li className="px-4">
+              <a href="#">GENERAL CONDITIONS OF USE</a>
+          </li>
+          <li className="px-4">
+              <a href="#">Cookie settings</a>
             </li>
           </ul>
-       
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-          <a href="https://www.facebook.com" className="text-white">
-            <FaFacebook className="w-6 h-6 mx-3" />
+       <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+          <a href="https://www.facebook.com" className="text-black">
+          <FaFacebook className="w-6 h-6 mx-3" />
           </a>
-          <a href="https://www.twitter.com" className="text-white">
-            <FaTwitter className="w-6 h-6 mx-3" />
-          </a>
-          <a href="https://www.instagram.com" className="text-white">
+          <a href="https://www.instagram.com" className="text-black">
             <FaInstagram className="w-6 h-6 mx-3" />
           </a>
-          <a href="https://www.linkedin.com" className="text-white">
+          <a href="https://www.linkedin.com" className="text-black">
             <FaLinkedin className="w-6 h-6 mx-3" />
           </a>
-        </span>
+          </span> 
+          </div>
       </div>
-    </footer>
-  </div>;
+
+  ;
 };
 
 export default Footer;
--- src/componets/Hero/NavBar.jsx
+++ src/componets/Hero/NavBar.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-      <div 
-      >NavBar</div>
+    <div >
+      <header className="p-[500px] w-1280 h-[100px] py-0">
+     
+        <div className="inline-flex  justify-start items-center w-100 py-8 px-8 text-black">
+      <div className='hidden lg:flex text-3xl items-center'
+          >
+            <img src="/public/products/logo-hennessy.jpg"></img>
+          </div>
+          <ul className="flex items-center gap-4 py-2 mx-auto ">
+            <li className="px-10 ">
+              <a href="#"> HOME</a>
+            </li>
+            <li className="px-10">
+              <a href="#">COLLECTION</a>
+          </li>
+          <li className="px-10">
+              <a href="#">ABOUT</a>
+          </li>
+          <li className="px-10">
+              <a href="#">TESTIMONIAL</a>
+          </li>
+          <li className="px-10">
+              <a href="#">CONTACT</a>
+            </li>
+        </ul>
+        </div>
+      </header>
+     <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+          <a href="https://www.facebook.com" className="text-black">
+          <FaFacebook className="w-6 h-6 mx-3" />
+          </a>
+          <a href="https://www.instagram.com" className="text-black">
+            <FaInstagram className="w-6 h-6 mx-3" />
+           </a>
+          </span> </div>
   )
 }
 
