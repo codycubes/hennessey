7ca5b4b2b415e5e8f300dea8a945ea25a1327592
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-23 11:55:44 +0200

    Products: Initial design

--- src/componets/Products/Products.jsx
+++ src/componets/Products/Products.jsx
@@ -1,7 +1,19 @@
+import SlideCard from "../SlideCard/SlideCard";
 import React from "react";
 
 const Products = () => {
-  return <div></div>;
+  return (
+    <section className="h-screen flex flex-col justify-center ">
+      <section className="flex flex-col justify-center sm:flex-row sm:items-center p-5 border-2 border-green-600">
+        <section className="border-2 border-black p-4 h-[60%] sm:flex sm:flex-col justify-center">
+          <section className="mb-4">The Collection</section>
+          <section>The Art of Cognac</section>
+          <button>Discover the entire collection</button>
+        </section>
+        <SlideCard />
+      </section>
+    </section>
+  );
 };
 
 export default Products;
---
+++ src/componets/SlideCard/SlideCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SlideCard = () => {
+  return (
+    <section className="border-2 border-red-500 flex flex-col items-center">
+      <section>
+        <img src="products/1.PNG" alt="" />
+      </section>
+      <section className="p-4">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. accusamus?
+      </section>
+    </section>
+  );
+};
+
+export default SlideCard;
