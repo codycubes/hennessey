96f3aee74dfef72d18080368b3af67bad78f03e1
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-26 13:14:34 +0200

    ProductCard created

---
+++ src/componets/Products/ProductCard.jsx
@@ -0,0 +1,20 @@
+const ProductCard = () => {
+  return (
+    <section className="h-[90%] border-2">
+      <section className="h-[90%] border-2">
+        <img
+          src="/31.jpg"
+          alt=""
+          className="h-full w-full object-fill object-center"
+        />
+      </section>
+      <p className="text-center p-5 h-[10%] border-2 flex items-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor obcaecati
+        labore fugiat voluptates illum nisi similique excepturi temporibus
+        architecto molestias!
+      </p>
+    </section>
+  );
+};
+
+export default ProductCard;
--- src/componets/Products/Products.jsx
+++ src/componets/Products/Products.jsx
@@ -1,9 +1,19 @@
+import ProductCard from "./ProductCard";
+
 const Products = () => {
   return (
     <section className="h-screen border-2">
-      <section className="h-full border-2 border-red-500 md:flex md:items-center">
-        <section className=" h-[20%] w-full border-2 border-blue-500 md:w-[40%] lg:w-[30%]"></section>
-        <section className="h-[80%] w-full border-2 border-green-500 md:w-[60%] lg:w-[70%] lg:h-full"></section>
+      <section className="h-full border-2  border-red-500 md:flex md:items-center lg:justify-center">
+        <section className=" min-h-[20%] w-full  border-2 border-blue-500 md: md:w-[40%] md:max-w-[550px]">
+          <section className="border flex flex-col p-4 gap-2 items-center sm:items-start">
+            <h2 className="text-4xl sm:text-6xl lg:text-7xl">The Collection</h2>
+            <h3 className="text-2xl">The Art of Congac</h3>
+            <button className="py-4 px-2 w-[150px]">Discover more</button>
+          </section>
+        </section>
+        <section className="flex flex-col h-[80%] w-full  border-2 border-green-500 md:w-[60%] lg:w-[750px] lg:h-full justify-center">
+          <ProductCard />
+        </section>
       </section>
     </section>
   );
